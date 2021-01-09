4abe8887c5d3052fa282523097d89728d6678225
FarisKadir <faris.kadir@gmail.com>
2021-01-09 15:45:53 -0500

    Finished initial layout of HTML file.

---
+++ script.js
@@ -0,0 +1,14 @@
+<div class="col-md-3 position-relative mt-5">
+                <div class="row">
+                    <p class="h2 bg-warning text-center">Timer</p>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p class="text-left fw-bold mb-0">Time Left:</p>
+                        <p class="text-left fw-bold mb-0">Incorrect:</p>
+                    </div>
+                    <div class="col">
+                        <p class="text-left fw-bold mb-0 mr-0" id="timer">56</p>
+                        <p class="text-left fw-bold mb-0 mr-0 pl-0" id="incorrect"></p>
+                    </div>
+                </div>
