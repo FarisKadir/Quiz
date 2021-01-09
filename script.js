23f7df6bd647395188babe334d89fb51a888227d
FarisKadir <faris.kadir@gmail.com>
2021-01-09 16:58:32 -0500

    Added an array of objects to contain my questions and answers. Added an event listener for the submit button.

--- script.js
+++ script.js
@@ -6,5 +6,48 @@ var submitBtn = document.querySelector("#submitBtn");
 var timer = 10;
 var score = 0;
 var incorrectCount = 0;
-var questions = [];
+var questionsArr = [
+    {
+        question: "How old are you?",
+        answerOne: "1",
+        answerTwo: "2",
+        answerThree: "3",
+        answerFour: "4",
+        correct: "5",
+    },
+    {
+        question: "Where are you from?",
+        answerOne: "1",
+        answerTwo: "2",
+        answerThree: "3",
+        answerFour: "4",
+        correct: "5",
+    },
+    {
+        question: "When were you born?",
+        answerOne: "",
+        answerTwo: "",
+        answerThree: "",
+        answerFour: "",
+        correct: "",
+    }
+];
+
+function printArr(obj)  {
+    console.log(obj.question);
+}
+
+submitBtn.addEventListener("click",/*run function */);
+
+
+questionsArr.forEach(printArr) {
+    console.log(""obj.question"");
+});
+
+
+// questionsArr.forEach(function(element,index,arr)    {
+//     console.log(arr.element);
+
+// });
+
 
