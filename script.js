193f3fe4aa1620dd80edc10c4f1f0c47bf62c374
FarisKadir <faris.kadir@gmail.com>
2021-01-09 16:03:04 -0500

    Removed ids from rows in html files. Created variables in script.js file to reference the elements in the html file.

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-<div class="col-md-3 position-relative mt-5">
-                <div class="row">
-                    <p class="h2 bg-warning text-center">Timer</p>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <p class="text-left fw-bold mb-0">Time Left:</p>
-                        <p class="text-left fw-bold mb-0">Incorrect:</p>
-                    </div>
-                    <div class="col">
-                        <p class="text-left fw-bold mb-0 mr-0" id="timer">56</p>
-                        <p class="text-left fw-bold mb-0 mr-0 pl-0" id="incorrect"></p>
-                    </div>
-                </div>
+var timer = document.querySelector("#timer");
+var title = document.querySelector("#quiztitle");
+var questions = document.querySelector("#question");
+var submitBtn = document.querySelector("#submitBtn");
+
+var timer = 10;
+var score = 0;
+var incorrectCount = 0;
+var questions = [];
+
