0a783a051a46b2738ee3146b34359693156a8c68
FarisKadir <faris.kadir@gmail.com>
2021-01-12 21:50:18 -0500

    Refactored choosing a random question, displaying the question, and creating buttons for the answers

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ var title = document.querySelector("#quiztitle");
 var questionArea = document.querySelector("#questionArea");
 var answers = document.getElementById("answers");
 var submitBtn = document.querySelector("#submitBtn");
+var progress = "";
+var button1 = document.querySelector("button1");
+var button2 = document.querySelector("button2");
+var button3 = document.querySelector("button3");
+var button4 = document.querySelector("button4");
 
-var choice = [];
-var timeLeft = 5;
+
+var timeLeft = 60;
 var score = 0;
 var incorrectCount = 0;
 var gameover = false;
@@ -33,14 +38,14 @@ var questionsArr = [
         answerFour: "The &lt;title&gt; section",
     },
     {
-        question: "What is the correct syntax for referring to an external script called &quot;xxx.js&quot;?",
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
         correct: "&lt;script src = &quot;xxx.js&quot;&gt;",
         answerTwo: "&lt;script href = &quot;xxx.js&quot;&gt;",
         answerThree: "&lt;script name = &quot;xxx.js&quot;&gt;",
         answerFour: "&lt;link script = &quot;xxx.js&quot;&gt;",
     },
     {
-        question: "How do you write &quot;Hello World&quot; in an alert box?",
+        question: "How do you write 'Hello World' in an alert box?",
         answerOne: "msgBox(&quot;Hello World&quot;)",
         correct: "alert(&quot;Hello World&quot;)",
         answerThree: "msg(&quot;Hello World&quot;)",
@@ -48,69 +53,72 @@ var questionsArr = [
     }
 ];
 
-//This is the function for the countdown timer
 
 
-
-
-
-//Randomly chooses a question and puts the object into variables.
-function ranQues(arr)  {
-    choice = arr[Math.floor(Math.random() * arr.length)];
-    console.log("This is from the ranQues function: " + JSON.stringify(choice));
-    return choice;
-}
-
-//This will display the questions and answers on the page.
-function dispQuest(choice)    {
-    console.log("Question: " + choice.question + "\nAnswer1: " + choice.answerOne + "\nAnswer2: " + choice.answerTwo + "\nAnswer3: " + choice.answerThree + "\nAnswer4: " + choice.answerFour + "\nCorrect Answer: " + choice.correct);
-    
-    // //Display Question
-    questionArea.innerText = choice.question;
-    // //End Display Question
-}
+//Timer
 
     
 
-    
-//This function creates a button for each answer.    
-    function lisAns()   {
-        for (var i = 1; i < 5; i++) {
-            var answer = choice[Object.keys(choice)[i]];
-            var entry = document.createElement("li");
-            var btn = document.createElement("button");
-            entry.appendChild(btn);
-            answers.appendChild(entry);
-            btn.innerHTML = i + ". " + answer;
-            btn.className = "btn btn-primary m-2 p-1";
-            btn.id = "answer" + i;
-        }
+//Randomly selects a question from the array and displays the questions and answers on the screen. It will remove the question from the array once it's displayed so that the same question doesn't get picked again.
+
+function assignQues(arr)    {
+    //Randomly choose an object from an array. The objects are the questions and answers.
+    randoChoice = arr[Math.floor(Math.random() * arr.length + 1)];
+
+    //Display Question
+    questionArea.innerText = randoChoice.question;
+    //End Display Question
+
+    for (var i = 1; i < 5; i++) {
+        var idx = i;
+        var key = Object.keys(randoChoice)[idx];
+        var value = randoChoice[key];
+
+        var answer = value;
+        var btnID = key;
+        var entry = document.createElement("li");
+        var btn = document.createElement("button");
+        
+        entry.appendChild(btn);
+        answers.appendChild(entry);
+        btn.innerHTML = i + ". " + answer;
+        btn.className = "btn btn-primary m-2 p-1";
+        btn.id = btnID;
     }
-
+}
 
 
 function startQuiz()  {
-    submitBtn.remove();
+    submitBtn.remove(); 
     title.remove();
-    ranQues(questionsArr);
-    dispQuest(choice);
-    lisAns();
-
-    //This is the countdown timer
-    setInterval(function()  {
+    //Timer
+    countdown = setInterval(function()  {
         if (timeLeft > 0)   {
             timeLeft--; 
             document.getElementById("timer").textContent = "Timer: " + timeLeft;
             console.log("The time left on the clock is " + timeLeft);
         }
+        else {
+            questionArea.textContent = "Time's Up!";
+            answers.remove();
+        }
     }, 1000);
+
+    assignQues(questionsArr);
+    
 }
+
+
+
+
+
 
 
 
 
 
 submitBtn.addEventListener("click",startQuiz);
+
 
 
 
