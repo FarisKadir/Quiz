55b9b8309f66bbb947986908dd838c7961caf5d0
FarisKadir <faris.kadir@gmail.com>
2021-01-13 18:29:16 -0500

    Testing out logic to use a while loop to control the criteria of the game.

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 var timer = document.querySelector("#timer");
-var title = document.querySelector("#quiztitle");
+// var title = document.querySelector("#quiztitle");
 var questionArea = document.querySelector("#questionArea");
 var answers = document.getElementById("answers");
-var submitBtn = document.querySelector("#submitBtn");
-var rmvBtn = document.querySelector("#btnAns");
-
-
-var timeLeft = 60;
+// var submitBtn = document.querySelector("#submitBtn");
+var count = 0;
 var score = 0;
-var incorrectCount = 0;
+var timeLeft = 30;
 var gameover = false;
-var questionCount = 0;
+
 
 var questionsArr = [
     {
@@ -50,6 +47,10 @@ var questionsArr = [
     }
 ];
 
+
+
+
+
 //Creates buttons for each answer
 function createBtns()   {
     for (var i = 1; i < 5; i++) {
@@ -59,33 +60,30 @@ function createBtns()   {
         var answer = value;
         var btnID = key;
         var entry = document.createElement("li");
-        var btn = document.createElement("buttonAns");
+        var btn = document.createElement("button");
         
         entry.appendChild(btn);
         answers.appendChild(entry);
         btn.innerHTML = i + ". " + answer;
-        btn.className = "btn btn-primary m-2 p-1 " + "btnAns";
+        btn.className = "btn btn-primary m-2 p-1";
         btn.setAttribute("onClick","checkAns(this.id)");
         btn.id = btnID;
     };
 };
 
+
 //Randomly selects a question from the array and displays the questions and answers on the screen. It will remove the question from the array once it's displayed so that the same question doesn't get picked again.
 
 function assignQues(arr)    {
-    console.log(arr);
-
-    
-    //Need to figure out how to remove existing buttons or not use append to create the buttons.
-
+    count++;
     //Randomly choose an object from an array. The objects are the questions and answers.
     ranInt = Math.floor(Math.random() * arr.length)
     randoChoice = arr[ranInt];
 
-    //Display Question
+    //Displays Question
     questionArea.innerText = randoChoice.question;
     
-    //Removes any existing buttons.
+    //Removes any existing buttons, if there are any, then creates new buttons.
     
     if (answers.hasChildNodes)    {
         answers.querySelectorAll('*').forEach(n => n.remove());
@@ -95,50 +93,78 @@ function assignQues(arr)    {
         createBtns();
     };
     
-    //Removes the question from the array
+    //Removes the question from the array so it doesn't get selected again
     arr.splice(ranInt, 1);
-    console.log(arr);
-
-}
+};
 
 //This function checks if the answer is right or wrong.
 function checkAns(btn_id)   {
-    questionsArr = questionsArr;
+    
     if (btn_id === "correct")   {
-        score++
-        console.log(score);
+        score++;
+        setScore
         timeLeft = timeLeft + 10;
         document.querySelector(".result").innerHTML = "You got it correct!";
         assignQues(questionsArr);
     }
     else    {
-        document.querySelector(".result").innerHTML = "You got Wrong!";
+        document.querySelector(".result").innerHTML = "You got the last one wrong!";
         timeLeft = timeLeft - 10;
         assignQues(questionsArr);
     }
 };
 
+function startQuiz()	{
+	document.querySelector("#submitBtn").remove();
+	document.querySelector("#quiztitle").remove();
+	while(timeLeft > 0 && score !== 5 && count !== questionsArr.length)	{
+			countdown = setInterval(function()  {
+			if (timeLeft > 0)   {
+				timeLeft--; 
+				document.getElementById("timer").textContent = "Timer: " + timeLeft;
+			}
+			else {
+				questionArea.innerText = "Time's Up!";
+				answers.remove();
+			}
+		}, 1000);
+		assignQues(questionsArr);
+	}
+	
+	questionArea.innerText = "Game Over!";
+	var name = "";
+	
+};
 
-function startQuiz()  {
-    submitBtn.remove(); 
-    title.remove();
-    //Timer
-    countdown = setInterval(function()  {
-        if (timeLeft > 0)   {
-            timeLeft--; 
-            document.getElementById("timer").textContent = "Timer: " + timeLeft;
-        }
-        else {
-            questionArea.textContent = "Time's Up!";
-            answers.remove();
-        }
-    }, 1000);
+//starts the quiz
+// function startQuiz()  {
+    // submitBtn.remove(); 
+    // title.remove();
 
-    assignQues(questionsArr);
-   
+    // //Timer
+    // countdown = setInterval(function()  {
+        // if (timeLeft > 0)   {
+            // timeLeft--; 
+            // document.getElementById("timer").textContent = "Timer: " + timeLeft;
+        // }
+        // else {
+            // questionArea.textContent = "Time's Up!";
+            // answers.remove();
+        // }
+    // }, 1000);
 
-    
-}
+
+// }
+
+
+
+
+
+
+
+
+
+
 
 
 
