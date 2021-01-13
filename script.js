06c042a91876c17a9cd0398027d20a55c8061d4c
FarisKadir <faris.kadir@gmail.com>
2021-01-13 17:30:00 -0500

    Included code that removes any buttons, if buttons already exist.

--- script.js
+++ script.js
@@ -3,13 +3,7 @@ var title = document.querySelector("#quiztitle");
 var questionArea = document.querySelector("#questionArea");
 var answers = document.getElementById("answers");
 var submitBtn = document.querySelector("#submitBtn");
-
-//Answer buttons
-var button1 = document.querySelector("#btn1");
-var button2 = document.querySelector("#btn2");
-var button3 = document.querySelector("#btn3");
-var button4 = document.querySelector("#btn4");
-
+var rmvBtn = document.querySelector("#btnAns");
 
 
 var timeLeft = 60;
@@ -56,13 +50,34 @@ var questionsArr = [
     }
 ];
 
-
+//Creates buttons for each answer
+function createBtns()   {
+    for (var i = 1; i < 5; i++) {
+        var idx = i;
+        var key = Object.keys(randoChoice)[idx];
+        var value = randoChoice[key];
+        var answer = value;
+        var btnID = key;
+        var entry = document.createElement("li");
+        var btn = document.createElement("buttonAns");
+        
+        entry.appendChild(btn);
+        answers.appendChild(entry);
+        btn.innerHTML = i + ". " + answer;
+        btn.className = "btn btn-primary m-2 p-1 " + "btnAns";
+        btn.setAttribute("onClick","checkAns(this.id)");
+        btn.id = btnID;
+    };
+};
 
 //Randomly selects a question from the array and displays the questions and answers on the screen. It will remove the question from the array once it's displayed so that the same question doesn't get picked again.
 
 function assignQues(arr)    {
     console.log(arr);
+
+    
     //Need to figure out how to remove existing buttons or not use append to create the buttons.
+
     //Randomly choose an object from an array. The objects are the questions and answers.
     ranInt = Math.floor(Math.random() * arr.length)
     randoChoice = arr[ranInt];
@@ -70,36 +85,25 @@ function assignQues(arr)    {
     //Display Question
     questionArea.innerText = randoChoice.question;
     
-    //Creates buttons for each answer
-    for (var i = 1; i < 5; i++) {
-        var idx = i;
-        var key = Object.keys(randoChoice)[idx];
-        var value = randoChoice[key];
-
-        var answer = value;
-        var btnID = key;
-        var entry = document.createElement("li");
-        var btn = document.createElement("button");
-        
-        entry.appendChild(btn);
-        answers.appendChild(entry);
-        btn.innerHTML = i + ". " + answer;
-        btn.className = "btn btn-primary m-2 p-1 " + "btn" + i;
-        btn.setAttribute("onClick","checkAns(this.id)");
-        btn.id = btnID;
-    }
-
+    //Removes any existing buttons.
+    
+    if (answers.hasChildNodes)    {
+        answers.querySelectorAll('*').forEach(n => n.remove());
+        createBtns();
+    }   
+    else {
+        createBtns();
+    };
+    
     //Removes the question from the array
     arr.splice(ranInt, 1);
     console.log(arr);
 
 }
 
-//This function checks the ID of an on click even of a button and checks if the answer is right or wrong.
+//This function checks if the answer is right or wrong.
 function checkAns(btn_id)   {
     questionsArr = questionsArr;
-    console.log(questionsArr);
-    console.log(btn_id);
     if (btn_id === "correct")   {
         score++
         console.log(score);
@@ -112,7 +116,7 @@ function checkAns(btn_id)   {
         timeLeft = timeLeft - 10;
         assignQues(questionsArr);
     }
-}
+};
 
 
 function startQuiz()  {
@@ -141,7 +145,6 @@ function startQuiz()  {
 
 
 submitBtn.addEventListener("click",startQuiz);
-
 
 
 
